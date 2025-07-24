33097e6cac8095d287edfedd6a282264f0cd1db8
Robert Roman <rob@romancoding.com>
2025-07-24 00:20:20 -0500

    removes auth file

--- aws/eks_auth.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_eks_cluster_auth" "cluster_auth" {
-  name = aws_eks_cluster.my_cluster.name
-
-  depends_on = [
-    aws_eks_cluster.my_cluster,
-    aws_eks_node_group.my_node_group
-  ]
-}
